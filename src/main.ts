70be7c9ff398fedd90fe8918fc5ce705d890685f
ludeeus <joasoe@gmail.com>
2020-02-04 22:31:20 +0000

    Add addons

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,8 @@ import {
 } from "lit-element";
 import { HomeAssistant } from "custom-card-helpers";
 import { classMap } from "lit-html/directives/class-map";
+import "@material/mwc-select/mwc-select";
+import "@material/mwc-list/mwc-list-item";
 import "@ludeeus/colorlog";
 
 @customElement("ui-logs")
@@ -16,32 +18,52 @@ export class UiLogs extends LitElement {
   @property({ type: Object }) public hass!: HomeAssistant;
   @property() private _HALogs?: string;
   @property() private _SupervisorLogs?: string;
-  @property() private _selected: "ha" | "supervisor" = "ha";
+  @property() private _AddonLogs?: string;
+  @property() private _selected: "ha" | "supervisor" | "addon" = "ha";
+  @property() private _addons?: Object[];
   @query("#log") private _logElement?: HTMLElement;
 
-  private async _getLogs() {
+  private async _getAddons(): Promise<void> {
+    const response = await this.hass.callApi("GET", `hassio/addons`);
+    this._addons = (response as any).data.addons;
+  }
+
+  private async _setLogsHA(): Promise<void> {
     this._HALogs = await this.hass.callApi<string>("GET", `error_log`);
-    if (this.hass.config.components.includes("hassio")) {
-      this._SupervisorLogs = await this.hass.callApi<string>(
-        "GET",
-        "hassio/supervisor/logs"
-      );
-    }
+  }
+
+  private async _setLogsSupervisor(): Promise<void> {
+    this._SupervisorLogs = await this.hass.callApi<string>(
+      "GET",
+      "hassio/supervisor/logs"
+    );
+  }
+
+  private async _setLogsAddon(slug: string): Promise<void> {
+    this._AddonLogs = await this.hass.callApi(
+      "GET",
+      `hassio/addons/${slug}/logs`
+    );
   }
 
   private async _reload() {
-    await this._getLogs();
+    if (this._selected === "ha") await this._setLogsHA();
+    else if (this._selected === "supervisor") await this._setLogsSupervisor();
+    else if (this._selected === "addon") await this._getAddons();
     this._logElement?.scrollIntoView({ block: "end", behavior: "smooth" });
   }
 
-  private _changeTab(tab: "ha" | "supervisor") {
+  private async _ChangeTabAction(tab: "ha" | "supervisor" | "addon") {
     this._selected = tab;
     window.scrollTo(0, 0);
+    if (this._selected === "ha") await this._setLogsHA();
+    else if (this._selected === "supervisor") await this._setLogsSupervisor();
+    else if (this._selected === "addon") await this._getAddons();
   }
 
   async firstUpdated(changedProperties) {
     super.firstUpdated(changedProperties);
-    await this._getLogs();
+    await this._ChangeTabAction("ha");
   }
 
   protected render(): TemplateResult | void {
@@ -53,7 +75,7 @@ export class UiLogs extends LitElement {
               "toolbar-button": true,
               selected: this._selected === "ha"
             })}
-            @click=${() => this._changeTab("ha")}
+            @click=${() => this._ChangeTabAction("ha")}
           >
             Home Assistant
           </div>
@@ -64,9 +86,18 @@ export class UiLogs extends LitElement {
                     "toolbar-button": true,
                     selected: this._selected === "supervisor"
                   })}
-                  @click=${() => this._changeTab("supervisor")}
+                  @click=${() => this._ChangeTabAction("supervisor")}
                 >
                   Supervisor
+                </div>
+                <div
+                  class=${classMap({
+                    "toolbar-button": true,
+                    selected: this._selected === "addon"
+                  })}
+                  @click=${() => this._ChangeTabAction("addon")}
+                >
+                  Addons
                 </div>
               `
             : ""}
@@ -80,10 +111,33 @@ export class UiLogs extends LitElement {
                 <color-log .log=${this._HALogs || ""}></color-log>
               </div>
             `
-          : html`
+          : this._selected === "supervisor"
+          ? html`
               <div class="ha-log log" id="log">
                 <color-log
                   .log=${this._SupervisorLogs?.replace(/\\[\d*\w/g, "") || ""}
+                ></color-log>
+              </div>
+            `
+          : html`
+              <div class="ha-log log" id="log">
+                <mwc-select outlined label="Addon">
+                  <mwc-list-item selected value="" selected></mwc-list-item>
+                  ${this._addons?.map(addon => {
+                    if ((addon as any).installed) {
+                      return html`
+                        <mwc-list-item
+                          @click=${() =>
+                            this._setLogsAddon((addon as any).slug)}
+                          >${(addon as any).name}</mwc-list-item
+                        >
+                      `;
+                    }
+                    return;
+                  })}
+                </mwc-select>
+                <color-log
+                  .log=${this._AddonLogs?.replace(/\\[\d*\w/g, "") || ""}
                 ></color-log>
               </div>
             `}
@@ -93,6 +147,12 @@ export class UiLogs extends LitElement {
 
   static get styles() {
     return css`
+      mwc-select {
+        width: calc(100% - 16px);
+        margin: 8px;
+        --mdc-theme-primary: var(--primary-color);
+        --mdc-theme-text-primary-on-background: var(--primary-text-color);
+      }
       .log {
         margin: 8px;
         margin-top: 73px;
